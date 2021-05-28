b427fdb6103f1f70e9f9c8a5c87cffe397f57f0d
Eugene Lim <eugeneljh@hotmail.com>
2021-05-28 10:26:12 +0800

    End of section 6

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(), // forRoot() means it has some service or component that it needs to initialize along with the root module
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
